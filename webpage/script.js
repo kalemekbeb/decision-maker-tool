872c4c331285145295e02d94064fbba6d6b7cb9e
Luke Sewell <212961806+lukeinloop@users.noreply.github.com>
2026-03-24 13:38:23 -0500

    moved comments, function skeleton for script.js

--- webpage/script.js
+++ webpage/script.js
@@ -1,6 +1,17 @@
-document.getElementById("pTest").innerText = "is this thang working?"
-
 /* 
 TODO:
 Need basic logic for option comparison.
 */
+
+
+// Function for adding in the options
+function addOption(){
+
+}
+
+// Function for calculating best option based off weights and values
+function calculate(){
+
+}
+
+document.getElementById("pTest").innerText = "is this thang working?"
