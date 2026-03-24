1c3b4ae4511bca60e513a34f1ddbb1e0a576e5e5
Luke Sewell <212961806+lukeinloop@users.noreply.github.com>
2026-03-24 17:41:32 -0500

    Added best option calculation and labels

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,4 +3,7 @@
 Use inverse order
 
 ## 2026-03-24 Luke Sewell
+Added basic option calculation and labels to input boxes
+
+## 2026-03-24 Luke Sewell
 Added addOption button for multiple options and included places to put values. Added basic css skeleton.
--- webpage/script.js
+++ webpage/script.js
@@ -1,7 +1,13 @@
 /* 
 TODO:
-Need basic logic for option comparison.
+Add more weights, maybe importance?
 */
+
+const weights = {
+    cost: 1,
+    time: -1, // more time is worse
+    enjoyment: 2
+};
 
 
 // Function for adding in the options
@@ -12,9 +18,13 @@ function addOption(){
     div.className = "option";
 
     div.innerHTML= ` 
-        <input type="text" class="option-input" name="option" placeholder="Enter one of the options">
+        <label for="option">Option:</label>
+        <input type="text" class="option-input" name="option" placeholder="Enter one of the options" min="1" max="5">
+        <label for="cost">Cost (1-5):</label>
         <input type="number" class="option-input" name="cost" placeholder="Enter cost efficency of options (1-5) 5 being cheapest" min="1" max="5">
+        <label for="time">Time Commitment(1-5):</label>
         <input type="number" class="option-input" name="time" placeholder="Enter time commitment required (1-5) 5 being longest" min="1" max="5">
+        <label for="enjoyment">Enjoyment (1-5):</label>
         <input type="number" class="option-input" name="enjoyment" placeholder="Enter predicted level of enjoyment (1-5) 5 being most" min="1" max="5">
     `;
 
@@ -23,13 +33,45 @@ function addOption(){
 
 // Function for calculating best option based off weights and values
 function calculate(){
-    /* TODO:
-    Need a calculate button
-    Need to grab all option blocks from html
-    Read values from each option (for loop)
-    Create a basic scoring system
-    Keep track of the best option
-
+    /*
+    TODO: 
+    Clear past decision 
+    Make the output a little more pretty
     */
 
+    // Grab the container we're using (this is for display)
+    const container = document.getElementById("computation-container");
+
+    // Grab all the option divs
+    const options = document.querySelectorAll(".option")
+
+    // set variables to default
+    let bestScore = 0;
+    let bestOption = null;
+
+    // for each option div
+    options.forEach(option => { 
+        // grab the values in each box
+        const name = option.querySelector('input[name="option"]').value;
+        const cost = option.querySelector('input[name="cost"]').value;
+        const time = option.querySelector('input[name="time"]').value;
+        const enjoyment = option.querySelector('input[name="enjoyment"]').value;
+
+        // calculate the score
+        const score = (cost * weights.cost) + (time * weights.time) + (enjoyment * weights.enjoyment);
+
+        // if this score is better than the best score, make it the new best and keep track of name
+        if(score > bestScore){
+            bestScore = score;
+            bestOption = name;
+        }
+    
+    });
+
+    // display the best option
+    const resultBox = document.createElement("div");
+
+    resultBox.textContent = 'Best option: ' + bestOption;
+
+    container.appendChild(resultBox);
 }
