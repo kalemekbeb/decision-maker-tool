b15d9778c3474fb126d8944e24c14893db833e0d
Luke Sewell <212961806+lukeinloop@users.noreply.github.com>
2026-03-24 14:49:38 -0500

    Added the ability to add options and assign values also rearranged index.html and started the css file

--- webpage/script.js
+++ webpage/script.js
@@ -6,12 +6,22 @@ Need basic logic for option comparison.
 
 // Function for adding in the options
 function addOption(){
+    // TODO: Make sure this is best practice
+    const option = document.getElementById("options-container");
+    const div = document.createElement("div");
+    div.className = "option";
 
+    div.innerHTML= ` 
+        <input type="text" class="option-input" name="option" placeholder="Enter one of the options"></input>
+        <input type="number" class="option-input" name="cost" placeholder="Enter cost efficency of options (1-5) 5 being cheapest"></input>
+        <input type="number" class="option-input" name="time" placeholder="Enter time commitment required (1-5) 5 being longest"></input>
+        <input type="number" class="option-input" name="time" placeholder="Enter predicted level of enjoyment (1-5) 5 being most"></input>
+    `;
+
+    option.appendChild(div);
 }
 
 // Function for calculating best option based off weights and values
 function calculate(){
 
 }
-
-document.getElementById("pTest").innerText = "is this thang working?"
