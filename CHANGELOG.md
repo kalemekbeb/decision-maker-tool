99302e1ff84f4ee458332171494e4ce119720137
Luke Sewell <212961806+lukeinloop@users.noreply.github.com>
2026-03-24 16:56:33 -0500

    Bugfixes and added change log markdown

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Change Log  
+
+Use inverse order
+
+## 2026-03-24 Luke Sewell
+Added addOption button for multiple options and included places to put values. Added basic css skeleton.
--- webpage/script.js
+++ webpage/script.js
@@ -12,10 +12,10 @@ function addOption(){
     div.className = "option";
 
     div.innerHTML= ` 
-        <input type="text" class="option-input" name="option" placeholder="Enter one of the options"></input>
-        <input type="number" class="option-input" name="cost" placeholder="Enter cost efficency of options (1-5) 5 being cheapest"></input>
-        <input type="number" class="option-input" name="time" placeholder="Enter time commitment required (1-5) 5 being longest"></input>
-        <input type="number" class="option-input" name="time" placeholder="Enter predicted level of enjoyment (1-5) 5 being most"></input>
+        <input type="text" class="option-input" name="option" placeholder="Enter one of the options">
+        <input type="number" class="option-input" name="cost" placeholder="Enter cost efficency of options (1-5) 5 being cheapest" min="1" max="5">
+        <input type="number" class="option-input" name="time" placeholder="Enter time commitment required (1-5) 5 being longest" min="1" max="5">
+        <input type="number" class="option-input" name="enjoyment" placeholder="Enter predicted level of enjoyment (1-5) 5 being most" min="1" max="5">
     `;
 
     option.appendChild(div);
@@ -23,5 +23,13 @@ function addOption(){
 
 // Function for calculating best option based off weights and values
 function calculate(){
+    /* TODO:
+    Need a calculate button
+    Need to grab all option blocks from html
+    Read values from each option (for loop)
+    Create a basic scoring system
+    Keep track of the best option
+
+    */
 
 }
